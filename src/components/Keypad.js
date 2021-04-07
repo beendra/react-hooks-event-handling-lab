c90ab7987e42d41bdad48d38f54af44326200d8a
beendra <alexandra.ricardel@gmail.com>
2021-04-07 09:51:50 -0500

    commit tree 5452e549e87baec4c08da3b45abade6095239f24

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import React from "react";
+import EyesonMe from "./EyesOnMe";
+
+function Keypad(){
+
+    function change(){
+        console.log('Entering password...')
+    }
+
+    return (
+            <input type="password" onChange={change} />
+    )
+}
+
+export default Keypad;
